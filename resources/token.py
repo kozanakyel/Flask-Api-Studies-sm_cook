4e27b740ed419d67e2701637efb7c2bee7614f39
kozanakyel <kozanakyel@gmail.com>
2022-02-12 20:23:15 +0300

    Adding JWT with flask-wt-extended library and Postgresql database using with marshmallow library2

---
+++ resources/token.py
@@ -0,0 +1,58 @@
+from http import HTTPStatus
+from flask import request
+from flask import jsonify, make_response
+from flask_restful import Resource
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    jwt_required,
+    get_raw_jwt
+)
+
+from utils import check_password
+from models.user import User
+
+black_list = set()
+
+
+class RevokeResource(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']
+
+        black_list.add(jti)
+
+        return {'message': 'Succesfully logged out'}, HTTPStatus.OK
+
+
+class TokenResource(Resource):
+
+    def post(self):
+        data = request.get_json()
+
+        email = data.get('email')
+        password = data.get('password')
+        print(email, password)
+        user = User.get_by_email(email=email)
+        print(check_password(password, user.password), user.id)
+        if not user or not check_password(password, user.password):
+            print('burdayim', type(jsonify({'message': 'username or password is incorrect'})))
+            return make_response(jsonify({'message': 'username or password is incorrect'}), HTTPStatus.UNAUTHORIZED)
+
+        print('hoppallaa')
+        access_token = create_access_token(identity=user.id, fresh=True)
+        refresh_token = create_refresh_token(identity=user.id)
+
+        return make_response(jsonify({'access_token': access_token, 'refresh_token': refresh_token}), HTTPStatus.OK)
+
+
+class RefreshResource(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+
+        access_token = create_access_token(identity=current_user, fresh=False)
+
+        return make_response(jsonify({'access_token': access_token}), HTTPStatus.OK)
---
+++ resources/user.py
@@ -0,0 +1,90 @@
+from flask import request
+from flask import jsonify
+from flask_restful import Resource
+from flask import request
+from flask_restful import Resource
+from flask_jwt_extended import get_jwt_identity, jwt_required, jwt_optional
+from http import HTTPStatus
+
+from webargs import fields
+from webargs.flaskparser import use_kwargs
+
+from models.recipe import Recipe
+from models.user import User
+
+from schemas.recipe import RecipeSchema
+from schemas.user import UserSchema
+
+recipe_list_schema = RecipeSchema(many=True)
+user_schema = UserSchema()
+user_public_schema = UserSchema(exclude=('email',))
+
+
+class MeResource(Resource):
+    @jwt_required
+    def get(self):
+        user = User.get_by_id(id=get_jwt_identity())
+        return user_schema.dump(user).data, HTTPStatus.OK
+
+
+class UserListResource(Resource):
+    def post(self):
+        json_data = request.get_json()
+
+        data, errors = user_schema.load(data=json_data)
+
+        if errors:
+            return {'message': 'Validation errors', 'errors': errors}, HTTPStatus.BAD_REQUEST
+
+        if User.get_by_username(data.get('username')):
+            return {'message': 'USername already used'}, HTTPStatus.BAD_REQUEST
+
+        if User.get_by_email(data.get('email')):
+            return {'message': 'email already used'}, HTTPStatus.BAD_REQUEST
+
+        user = User(**data)
+        user.save()
+
+        return user_schema.dump(user).data, HTTPStatus.CREATED
+
+
+class UserResource(Resource):
+
+    @jwt_optional
+    def get(self, username):
+
+        user = User.get_by_username(username=username)
+
+        if user is None:
+            return jsonify({'message': 'user not found'}), HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user == user.id:
+            data = user_schema.dump(user).data
+        else:
+            data = user_public_schema.dump(user).data
+
+        return data, HTTPStatus.OK
+
+class UserRecipeListResource(Resource):
+
+    @jwt_optional
+    @use_kwargs({'visibility': fields.Str(missing='public')})
+    def get(self, username, visibility):
+
+        user = User.get_by_username(username=username)
+
+        if user is None:
+            return {'message': 'User not found'}, HTTPStatus.NOT_FOUND
+
+        current_user = get_jwt_identity()
+
+        if current_user == user.id and visibility in ['all', 'private']:
+            pass
+        else:
+            visibility = 'public'
+
+        recipes = Recipe.get_all_by_user(user_id=user.id, visibility=visibility)
+
+        return recipe_list_schema.dump(recipes).data, HTTPStatus.OK
