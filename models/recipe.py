691e67ef7f13f2477c95f53e55b8f4969df25021
kozanakyel <kozanakyel@gmail.com>
2022-02-15 19:24:23 +0300

    Sorting attribute for our API with created tyime and other params

--- models/recipe.py
+++ models/recipe.py
@@ -18,14 +18,19 @@ class Recipe(db.Model):
     user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
 
     @classmethod
-    def get_all_published(cls, q, page, per_page):
+    def get_all_published(cls, q, page, per_page, sort, order):
 
         keyword = '%{keyword}%'.format(keyword=q)
+
+        if order == 'asc':
+            sort_logic = asc(getattr(cls, sort))
+        else:
+            sort_logic = desc(getattr(cls, sort))
 
         return cls.query.filter(or_(cls.name.ilike(keyword),
                                     cls.description.ilike(keyword)),
                                 cls.is_publish.is_(True)). \
-            order_by(desc(cls.created_at)).paginate(page=page, per_page=per_page)
+            order_by(sort_logic).paginate(page=page, per_page=per_page)
 
     @classmethod
     def get_by_id(cls, recipe_id):
